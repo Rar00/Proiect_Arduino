a7a70edbb6867a57fa78a755c36813bd1a1a6ce8
Rar00 <preramarin@gmail.com>
2023-05-12 20:10:48 +0300

    modifiead gsm code

--- Cod_GSM_A6/Cod_GSM_A6_uno/Cod_GSM_A6_uno.ino
+++ Cod_GSM_A6/Cod_GSM_A6_uno/Cod_GSM_A6_uno.ino
@@ -11,22 +11,23 @@ void setup()
   //Begin serial communication with Arduino and A6
   mySerial.begin(9600);
 
-  Serial.println("Initializing...");
+  Serial.println("Initializing..."); 
   delay(1000);
-
+  mySerial.println("ok");
   mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
   updateSerial();
-  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-  updateSerial();
-  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-  updateSerial();
-  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
-  updateSerial();
+
+//  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+//  updateSerial();
+//  mySerial.println("AT+CMGS=\"+40729364858\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+//  updateSerial();
+//  mySerial.print("Last Minute Engineers | lastminuteengineers.com"); //text content
+//  updateSerial();
+//  mySerial.write(26);
 }
 
 void loop()
 {
-  updateSerial();
 }
 
 void updateSerial()
@@ -40,3 +41,4 @@ void updateSerial()
   {
     Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
   }
+}
